d111f597c2a4295af01769e59f8d5d40f77f85ae
OzcanI <ibosnfs1997@gmail.com>
2025-08-24 19:06:04 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             >
               <div className="relative">
                 <Image
-                  src="/images/logo.png"
+                  src="/spartaloglandingpage/images/logo.png"
                   alt="SpartaLog"
                   width={40}
                   height={40}
@@ -440,7 +440,7 @@ export default function Home() {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="flex items-center space-x-3 mb-4 md:mb-0">
               <Image
-                src="/images/logo.png"
+                src="/spartaloglandingpage/images/logo.png"
                 alt="SpartaLog"
                 width={32}
                 height={32}
